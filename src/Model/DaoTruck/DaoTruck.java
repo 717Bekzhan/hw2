f80a9da2f483a9406b9a38a4bc070a7a395d9fb0
Beksultan Mamatkadyr uulu <90905002+beksultanFromPeaksoft@users.noreply.github.com>
2022-04-17 17:27:29 +0600

    Update DaoTruck.java

--- src/Model/DaoTruck/DaoTruck.java
+++ src/Model/DaoTruck/DaoTruck.java
@@ -37,12 +37,12 @@ public class DaoTruck {
     }
 
     public void road(int ID) {
-        for (Map.Entry<Integer, Truck> k : daoTruck.entrySet()) {
+        for (Map.Entry<Integer, Truck> k : daoTruck.entrySet()) { // forgo salbai ele jon ele .get() degen methoddu chakyrsanyz bolmok
             if (k.getKey() == ID) {
                 if (k.getValue().getState().equals(State.ON_THE_ROAD)) {
                     System.err.println("These truck on the way ");
                 } else {
-                    if (k.getValue().getDriver() == null) {
+                    if (k.getValue().getDriver() == null) { // esli driver null bolso emne uchun driver bosh emes dep chygarat
                         System.out.println("Driver is not free!");
                     } else {
                         k.getValue().setState(State.ON_THE_ROAD);
@@ -51,20 +51,20 @@ public class DaoTruck {
                     }
                 }
             }
-        }
-    } public void repairing(int id) {
+        } // no clean code
+    } public void repairing(int id) { // repairden repairge kylsak kandai bolot
         daoTruck.entrySet().stream().filter(x -> x.getKey() == id).forEach(x -> x.getValue().setState(State.ON_THE_REPAIR));
         System.out.println("=================================================");
         System.out.println("you have successfully sent for repair");
     }
 
-    public void base(int id) {
+    public void base(int id) { // basedan basega jonotsok bolobu?
         daoTruck.entrySet().stream().filter(x -> x.getKey() == id).forEach(x -> x.getValue().setState(State.ON_THE_BASE));
         System.out.println("=================================================");
         System.out.println("you have successfully sent for base");
     }
 
-    public void showId(int id) {
+    public void showId(int id) { // jon ele .get() looptun orduna koldonsonuz jakshy bolmok
         for (Map.Entry<Integer, Truck> t : daoTruck.entrySet()) {
             if (id == t.getKey()) {
                 try {
